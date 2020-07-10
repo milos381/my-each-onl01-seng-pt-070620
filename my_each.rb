e9a9855d6b42c857701ddb113c6ee318c8ac8d19
Milos Popovic <milos_popovic@outlook.com>
2020-07-10 17:12:21 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+words = ["hello", "there"]
+def my_each(words)
+  i = 0
+  while i < words.length
+# The "while" method will always return a value of => nil"
+    yield(words[i])
+    i += 1
+  end
+  words
+end
+
+my_each(words){|i| "#{i}"}
+# Use the collection method
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+def new_method
+
 end
